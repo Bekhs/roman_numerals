d0bad32f3b254da69c7fd7181d1be317b400b8fa
Bekhs <bekhsyopta@gmal.com>
2020-12-13 20:12:38 +0500

    README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Roman numerals
 Roman numerals it's programm which converts numbers from arabic numerals to roman numerals and from roman numerals to arabic numerals
+---
+# Римские цифры
+Roman numerals это программа для конвертации чисел из римских в арабские и обратно
--- roman_numerals.py
+++ roman_numerals.py
@@ -1,17 +1,17 @@
-def to_roman_numerals(number):
+def to_roman_numerals(number): # fuction for converts numbers from arabic numerals to roman numerals
     pass
 
-def to_arabic_numerals(number):
+def to_arabic_numerals(number): # fuction for converts numbers from roman numerals to arabic numerals
     pass
 
-def main():
+def main(): # main function
     number = input('Write number: ')
-    try:
+    try: # try convert str to int
         number = int(number)
-        number = to_roman_numerals(number)
+        number = to_roman_numerals(number) # if number is int convert number to roman numerals
         print(number)
-    except ValueError:
-        number = to_arabic_numerals(number)
+    except ValueError: # else
+        number = to_arabic_numerals(number) # if number is not int convert number to arabic numerals
         print(number)
 
 if __name__ == '__main__':
