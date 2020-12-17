5c4ca1cd1bebdf96a55d141aed1db487b77fbada
Bekhs <bekhsyopta@gmal.com>
2020-12-17 19:57:24 +0500

    function to_arabic_numerals update

--- roman_numerals.py
+++ roman_numerals.py
@@ -2,7 +2,26 @@ def to_roman_numerals(number): # fuction for converts numbers from arabic numera
     pass
 
 def to_arabic_numerals(number): # fuction for converts numbers from roman numerals to arabic numerals
-    pass
+    try:
+        numbers = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'H':10000}
+        now = 'I'
+        n = 0
+        a = [None,0]
+        for x in number[::-1]:
+            if numbers[x] >= numbers[now]:
+                now = x
+                n += numbers[x]
+                a[1] = 0
+            else:
+                if a[1] > 1 or ( a[1] == 1 and a[0] != x ) or int(numbers[now] / numbers[x]) <= 2:
+                    return 'Error!'
+                else:
+                    a[0] = x
+                    a[1] += 1
+                    n -= numbers[x]
+        return n
+    except KeyError:
+        return 'Error!'
 
 def main(): # main function
     number = input('Write number: ')
@@ -11,6 +30,7 @@ def main(): # main function
         number = to_roman_numerals(number) # if number is int convert number to roman numerals
         print(number)
     except ValueError: # else
+        number = number.upper()
         number = to_arabic_numerals(number) # if number is not int convert number to arabic numerals
         print(number)
 
